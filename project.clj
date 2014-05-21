af31b673635f1b964c33c69aca66e2fc84b01f45
Jason Gilman <jason@element84.com>
2014-05-21 09:05:01 -0400

    Updating library versions. Adding standard out silencer to handle annoying http kit exceptions during restart.

--- project.clj
+++ project.clj
@@ -4,24 +4,24 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/timbre "2.6.1"]
-                 [compojure "1.1.5"]
-                 [ring-server "0.2.8" :exclusions [ring/ring-jetty-adapter]]
-                 [http-kit "2.1.10"]
-                 [cheshire "5.2.0"]
-                 [clj-wamp "1.0.0"]
-                 [hiccup "1.0.4"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.taoensso/timbre "3.2.1"]
+                 [compojure "1.1.8"]
+                 [ring-server "0.3.1" :exclusions [ring/ring-jetty-adapter]]
+                 [http-kit "2.1.18"]
+                 [cheshire "5.3.1"]
+                 [clj-wamp "1.0.2"]
+                 [hiccup "1.0.5"]
 
                  ; Clojurescript
-                 [hiccups "0.2.0"]
+                 [hiccups "0.3.0"]
 
                  ; Testing
-                 [clj-http "0.7.6"]
+                 [clj-http "0.9.1"]
                  ; Testing that includes the filesystem (project-viz)
                  [me.raynes/fs "1.4.5"]
                  ; Clojurescript testing
-                 [com.cemerick/clojurescript.test "0.0.4"]]
+                 [com.cemerick/clojurescript.test "0.3.0"]]
   :plugins [[lein-cljsbuild "0.3.2"]
             [codox "0.6.4"]]
 
--- src/clj/vdd_core/internal/system.clj
+++ src/clj/vdd_core/internal/system.clj
@@ -4,21 +4,50 @@
             [clojure.java.io :as io]
             [vdd-core.internal.routes :as routes]
             [taoensso.timbre :as timbre
-             :refer (trace debug info warn error fatal spy)]))
+             :refer (trace debug info warn error fatal spy)])
+  (:import [java.io
+            PrintStream
+            OutputStream]))
+
+(defn- silence-std-out
+  "Quiets standard out and standard error while executing the function"
+  [f]
+  (let [orig-out System/out
+        orig-err System/err
+        nothing-writer (PrintStream. (proxy [OutputStream] []
+                                       (write
+                                         ([b]
+                                              ;; does nothing
+                                              )
+
+                                       ([_ _ _]
+                                              ;;does nothing
+                                              ))
+                                       ))]
+    (System/setOut nothing-writer)
+    (System/setErr nothing-writer)
+    (try
+      (f)
+      (finally
+        (System/setOut orig-out)
+        (System/setErr orig-err)))))
 
 (defn- stop-server [server]
   (when-not (nil? server)
     (info "stopping server...")
-    ; contains the stop-server callback fn
-    (server))) 
+    ;; server contains the stop-server callback fn
+    ;; Hide the annoying java.util.concurrent.RejectedExecutionException displayed by HTTP Kit during restarts.
+    ;; See https://github.com/http-kit/http-kit/issues/91
+    ;; I tried all solutions but couldn't get any other way to work.
+    (silence-std-out #(server :timeout 2000))))
 
 (defn- start-server [config]
   (let [http-config {:ip "0.0.0.0"
                      :port (:port config)
                      :thread 2}
-        the-app (ring-reload/wrap-reload (routes/app config)) 
+        the-app (ring-reload/wrap-reload (routes/app config))
         server (httpkit/run-server the-app http-config)]
-    (info "server started. listen on" (:ip http-config) "@" (:port http-config))
+    (info "server started. listen on" (:ip http-config) "@" (:port http-config) "with config" (pr-str http-config))
     server))
 
 (defn system
@@ -33,14 +62,14 @@
   (let [log-config (:log config)]
     (timbre/set-level! (or (:level log-config) :warn))
     (timbre/set-config! [:timestamp-pattern] "yyyy-MM-dd HH:mm:ss")
-    
+
     ; Enable file logging
     (timbre/set-config! [:appenders :spit :enabled?] true)
-    (timbre/set-config! [:shared-appender-config :spit-filename] 
+    (timbre/set-config! [:shared-appender-config :spit-filename]
                         (:file log-config))
-    
+
     ; Enable/disable stdout logs
-    (timbre/set-config! [:appenders :standard-out :enabled?] 
+    (timbre/set-config! [:appenders :standard-out :enabled?]
                         (:stdout-enabled log-config))))
 
 (defn start
